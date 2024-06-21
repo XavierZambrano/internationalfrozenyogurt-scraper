7c690c720200c0df25d90785519dc38e0e87fa93
XavierZambrano <xavierzamco@gmail.com>
2024-06-21 01:54:51 -0600

    feat: add parse method

--- internationalfrozenyogurt/settings.py
+++ internationalfrozenyogurt/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "internationalfrozenyogurt.spiders"
 #USER_AGENT = "internationalfrozenyogurt (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- internationalfrozenyogurt/spiders/suppliers.py
+++ internationalfrozenyogurt/spiders/suppliers.py
@@ -1,10 +1,40 @@
 import scrapy
+from scrapy.http import TextResponse
+from bs4 import BeautifulSoup
 
 
 class SuppliersSpider(scrapy.Spider):
     name = "suppliers"
     allowed_domains = ["internationalfrozenyogurt.com"]
-    start_urls = ["https://internationalfrozenyogurt.com"]
+    start_urls = ["https://internationalfrozenyogurt.com/suppliers/"]
 
     def parse(self, response):
-        pass
+        next_page_url = response.xpath('//li[@class="bpn-next-link"]/a/@href').get()
+        if next_page_url:
+            yield scrapy.Request(next_page_url, callback=self.parse)
+
+        # some bug with the response.xpath('//article') is happening
+        suppliers_container = BeautifulSoup(response.body, 'html.parser').find_all('article')
+
+        for container in suppliers_container:
+            container = TextResponse(url=response.url, body=str(container), encoding='utf-8')
+
+            address = container.xpath('.//div[@class="contact-item supplier-address"]/text()').getall()
+            if address:
+                address = ','.join([line.strip() for line in address if line.strip() != ''])
+
+            phone = container.xpath('.//div[@class="contact-item supplier-phone"]/text()').get()
+            if phone:
+                phone = phone.strip()
+
+            supplier = {
+                'name': container.xpath('.//h3/a/text()').get(),
+                'url': container.xpath('.//h3/a/@href').get(),
+                'categories': container.xpath('.//div[@class="supplier-categories"]/span/a/text()').getall(),
+                'website': container.xpath('.//div[@class="contact-item supplier-website"]/a/@href').get(),
+                'phone': phone,
+                'address': address,
+            }
+            yield supplier
+
+
